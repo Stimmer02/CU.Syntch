d83c0a6dcfa0ea71f2f423c9fe4aec1c1240120a
emil.szerafn <mojzesz2002@gmail.com>
2023-11-16 16:00:25 +0100

    compile.sh patch fix

--- compile.sh
+++ compile.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+cd src || exit
+
 COMPILATION_COMMAND="g++ main.cpp UserInput/KeyboardInput_DevInput.cpp UserInput/TerminalInputDiscard.cpp UserInput/KeyboardRecorder_DevInput.cpp UserInput/KeyboardDoubleBuffer.cpp Synthesizer.cpp AudioPipelineSubstitute.cpp Synthesizer/Generator_Sine.cpp UserInput/keyboardTransferBuffer.cpp AudioOutput/OutStream_PulseAudio.cpp AudioOutput/audioBuffer.cpp UserInput/InputMap.cpp Pipeline/BufferConverter/BufferConverter_Mono8.cpp Pipeline/BufferConverter/BufferConverter_Mono16.cpp Pipeline/BufferConverter/BufferConverter_Mono24.cpp Pipeline/BufferConverter/BufferConverter_Mono32.cpp Pipeline/Statistics/PipelineStatisticsService.cpp -lpulse-simple -lpulse -ggdb"
 
 $($COMPILATION_COMMAND)
 
-if [ $? = 0 ] && [ "$1" = "-r" ]; then
-    ./a.out "$2"
+if [ $? = 0 ]; then
+    cd ..
+    mv ./src/a.out ./CPU-synth.out
+
+    if [ "$1" = "-r" ]; then
+        echo "running: ./CPU-synth $2"
+        ./CPU-synth.out "$2"
+    fi
 fi
+
+
