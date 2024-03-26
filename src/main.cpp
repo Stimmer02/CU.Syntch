068e8c89dedafa11d4d152ad0e052e6f2058800f
emil.szerafin <mojzesz2002@gmail.com>
2024-03-26 11:50:34 +0100

    main script input rework

--- src/main.cpp
+++ src/main.cpp
@@ -44,8 +44,6 @@ void initializeAudioInfoFromFile(audioFormatInfo& audioInfo, const std::string& 
 }
 
 
-void updateStatistics(const statistics::pipelineStatistics* pStatistics, const audioFormatInfo& audioInfo);
-
 int main(int argc, char** argv){
 
     const ushort keyCount = 127;
@@ -55,12 +53,12 @@ int main(int argc, char** argv){
     
 
 
-    if (argc == 2 && (std::strcmp("help", argv[1]) || std::strcmp("--help", argv[1]))){
+    if (argc > 3 && (std::strcmp("help", argv[1]) || std::strcmp("--help", argv[1]))){
         std::printf(
         "USAGE: \n"
-        "%s (no arguments)\n"
-        "%s <script path>\n"
-        "%s <MIDI file path> <WAV file out>\n",
+        "%s (no arguments)     - launches default script in user mode\n"
+        "%s <script path>      - launches script and closes at the end\n"
+        "%s user <script path> - launches script and stays open\n",
         argv[0], argv[0], argv[0]);
     } else if (argc == 1){
         pipeline::SynthUserInterface userInterface("./config/histSave.txt", audioInfo, keyCount);
@@ -77,21 +75,17 @@ int main(int argc, char** argv){
             return 1;
         }
     } else {
-        std::printf("Reading MIDI file: %s\n", argv[1]);
-
-        MIDI::MidiFileReader midiReader(argv[1] ,audioInfo.sampleSize, audioInfo.sampleRate);
-        pipeline::AudioPipelineManager audioPipeline(audioInfo, keyCount);
-        short synthID = audioPipeline.addSynthesizer();
-        audioPipeline.loadSynthConfig("./config/synth.config", synthID);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        if (audioPipeline.recordUntilStreamEmpty(midiReader, synthID, argv[2])){
-            return 2;
+        if (std::strcmp("user", argv[1])){
+            std::fprintf(stderr, "Invalid argument: %s\n", argv[1]);
+            return 1;
         }
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::printf("File successfully saved as: %s\n", argv[2]);
-        std::printf("Time elapsed: %f s\n", std::chrono::duration<double>(end-start).count());
+        pipeline::SynthUserInterface userInterface("./config/histSave.txt", audioInfo, keyCount);
+        std::printf("Executing script: %s\n", argv[2]);
+        if (userInterface.scriptReader.executeScript(argv[2], true)){
+            std::fprintf(stderr, "%s", userInterface.scriptReader.getLastError().c_str());
+            return 1;
+        }
+        userInterface.start();
     }
 
     return 0;
